379f880d2a53f58db937e4d38daa1ed5d0fd532c
Fred Hsu <fred.hsu@bomdic.com>
2017-01-11 10:08:49 +0800

    Modify App Wrapper

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import AppWrapper from './appWrapper';
 import configureStore from './configureStore';
 
 const store = configureStore();
 
 ReactDOM.render(
   <AppContainer>
-    <App store={store} />
+    <AppWrapper store={store} />
   </AppContainer>,
   document.querySelector('#app-entry-point')
 );
 
 if (module.hot) {
-  module.hot.accept('./app', () => {
-    const NextApp = require('./app').default;
+  module.hot.accept('./appWrapper', () => {
+    const NextApp = require('./appWrapper').default;
     ReactDOM.render(
       <NextApp store={store} />,
       document.querySelector('#app-entry-point')
