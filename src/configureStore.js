7c3c400d978e81edcfcc6b8d3f8a6f598d9201ee
Fred Hsu <fred.hsu@bomdic.com>
2017-01-13 10:53:41 +0800

    Rebuild Clear StoreConfigure

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,12 +2,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import rootReducer from './reducers';
-const createStoreWithMiddleware = applyMiddleware(
-  promise
-)(createStore);
 
 export default function configuredStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(promise)
+  );
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
