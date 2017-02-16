9c15b620454bdfb61bc485811a4363e769028d86
Fred Hsu <fred.hsu@bomdic.com>
2017-02-16 11:22:24 +0800

    Impliment Browser Redux Devtool

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,24 @@
 /* eslint global-require: 0*/
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import promise from 'redux-promise';
 import rootReducer from './reducers';
 
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+/* eslint-enable no-underscore-dangle */
+
+const enhancer = composeEnhancers(
+  applyMiddleware(promise)
+);
+
 export default function configuredStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(promise)
-  );
+  const store = createStore(rootReducer, initialState, enhancer);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
